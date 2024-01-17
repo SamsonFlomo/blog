0c8b3c13d4b621b3702a9afc9f7f7145ed9bf551
SamsonFlomo <flomosamson125@gmail.com>
2024-01-17 17:08:22 +0000

    contact form(jotform)

--- assets/js/main.js
+++ assets/js/main.js
@@ -260,66 +260,6 @@ search.addEventListener('keyup', () => {
     }
 });
 
-
-// Email
-const form = document.getElementById("contact-form");
-const fullName = document.getElementById("name");
-const email = document.getElementById("email");
-const subject = document.getElementById("subject");
-const phone = document.getElementById("phone");
-const message = document.getElementById("message-id");
-
-function sendEmail() {
-    Swal.fire({
-        title: "Success!",
-        text: "Message sent successfully!",
-        icon: "success"
-    });
-
-            form.submit();
-            form.reset();
-}
-
-function checkInputs() {
-    const items = document.querySelectorAll(".item");
-
-    for (const item of items) {
-        if (item.value == "") {
-            item.classList.add("error");
-            item.parentElement.classList.add("error");
-        }
-
-        item.addEventListener("keyup", () => {
-            if (item.value != "") {
-                item.classList.remove("error");
-                item.parentElement.classList.remove("error");
-            } else {
-                item.classList.add("error");
-                item.parentElement.classList.add("error");
-            }
-        });
-    }
-}
-
-document.addEventListener("DOMContentLoaded", function () {
-    form.addEventListener("submit", (e) => {
-        e.preventDefault(); // Prevent the default form submission
-
-        checkInputs();
-
-        if (
-            !fullName.classList.contains("error") &&
-            !email.classList.contains("error") &&
-            !phone.classList.contains("error") &&
-            !subject.classList.contains("error") &&
-            !message.classList.contains("error")
-        ) {
-            sendEmail();
-        }
-    });
-});
-
-
 // Quote js
     const quote = document.getElementById("quote");
     const author_q = document.getElementById("author");
